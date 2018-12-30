823d669519891f62ff20567335c7ccdd7dbc891a
Armin Kunkel <armin@kunkel24.de>
2018-12-30 19:32:46 +0100

    Add follow logic stub

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,15 @@ const express = require('express'),
       crypto = require('crypto'),
       generateRSAKeypair = require('generate-rsa-keypair');
 
+const Webfinger = require('webfinger.js');
+
+const webfinger = new Webfinger({
+    webfist_fallback: false,  // defaults to false
+    tls_only: false,          // defaults to true
+    uri_fallback: false,     // defaults to false
+    request_timeout: 10000,
+});
+
 function createActor(name, domain, pubkey) {
   return {
     '@context': [
@@ -15,7 +24,7 @@ function createActor(name, domain, pubkey) {
     'type': 'Person',
     'preferredUsername': `${name}`,
     'inbox': `https://${domain}/api/inbox`,
-
+    'followers': `https://${domain}/u/${name}/followers`,
     'publicKey': {
       'id': `https://${domain}/u/${name}#main-key`,
       'owner': `https://${domain}/u/${name}`,
@@ -38,6 +47,35 @@ function createWebfinger(name, domain) {
   };
 }
 
+function createFollowerCollection(name, domain) {
+  return {
+      "@context": "https://www.w3.org/ns/activitystreams",
+      "id": `https://${domain}/u/${name}/followers`,
+      "summary": `${name}\'s followers collection`,
+      "type": "OrderedCollection",
+      "totalItems": 1,
+      "orderedItems": [
+          {
+              '@context': [
+                  'https://www.w3.org/ns/activitystreams',
+                  'https://w3id.org/security/v1'
+              ],
+
+              'id': `https://${domain}/u/${name}`,
+              'type': 'Person',
+              'preferredUsername': `${name}`,
+              'inbox': `https://${domain}/api/inbox`,
+              'followers': `https://${domain}/u/${name}/followers`,
+              'publicKey': {
+                  'id': `https://${domain}/u/${name}#main-key`,
+                  'owner': `https://${domain}/u/${name}`,
+                  'publicKeyPem': pubkey
+              }
+        }
+      ]
+  }
+}
+
 router.post('/create', function (req, res) {
   // pass in a name for an account, if the account doesn't exist, create it!
   const account = req.body.account;
@@ -50,6 +88,7 @@ router.post('/create', function (req, res) {
   var pair = generateRSAKeypair();
   let actorRecord = createActor(account, domain, pair.public);
   let webfingerRecord = createWebfinger(account, domain);
+  //const followerCollection = createFollowerCollection(account, domain);
   const apikey = crypto.randomBytes(16).toString('hex');
   db.run('insert or replace into accounts(name, actor, apikey, pubkey, privkey, webfinger) values($name, $actor, $apikey, $pubkey, $privkey, $webfinger)', {
     $name: `${account}@${domain}`,
@@ -59,8 +98,38 @@ router.post('/create', function (req, res) {
     $actor: JSON.stringify(actorRecord),
     $webfinger: JSON.stringify(webfingerRecord)
   }, (err, accounts) => {
-    res.status(200).json({msg: 'ok', apikey});
+      console.log(JSON.stringify(err, null, 2))
+      console.log(JSON.stringify(accounts, null, 2))
+      res.status(200).json({msg: 'ok', apikey});
   });
 });
+
+router.post('/follow', (req, res) => {
+    let db = req.app.get('db');
+    let domain = req.app.get('domain');
+    let acct = req.body.acct;
+    let apikey = req.body.apiKey;
+    let wantToFollow = req.body.wantToFollow;
+    // check to see if your API key matches
+    db.get('select apikey from accounts where name = $name', {$name: `${acct}`}, (err, result) => {
+        if (result.apikey === apikey) {
+            follow(acct, wantToFollow)
+        }
+        else {
+            res.status(403).json({msg: 'wrong api key'});
+        }
+    });
+})
+
+function follow(acct, wantToFollow) {
+    // user is allowed to use api
+    webfinger.lookup(wantToFollow, function (err, p) {
+        if (err) {
+            console.log('error: ', err.message);
+        } else {
+            console.log(p);
+        }
+    });
+}
 
 module.exports = router;
